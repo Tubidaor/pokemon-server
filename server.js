3d49fc7d1d5eaf70643eb8f208c87b3aa4983ff7
Juan Baltazar <juan.baltazar1@gmail.com>
2019-12-01 22:03:43 -0800

    added pokemon and type endpoints to server

--- server.js
+++ server.js
@@ -1,17 +1,56 @@
+require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
+const POKEDEX = require('./pokedex.json');
 
 const app = express();
 
 app.use(morgan('dev'));
 
-app.use((req, res) => {
-  res.send('Hello world, from Tampa, Florida airport')
-})
+app.use(function validateBearerToken(req, res, next) {
+  const apiToken = process.env.API_TOKEN
+  const authToken = req.get('Authorization')
+  console.log(authToken)
+  
+  if (!authToken || authToken.split(' ')[1] !== apiToken) {
+    return res.status(401).json({ error: 'Unauthorized request' })
+  }
+
+
+  // move to the next middleware
+  next()
+  })
+
+const validTypes = [`Bug`, `Dark`, `Dragon`, `Electric`, `Fairy`, `Fighting`, `Fire`, `Flying`, `Ghost`, `Grass`, `Ground`, `Ice`, `Normal`, `Poison`, `Psychic`, `Rock`, `Steel`, `Water`]
+function handleGetTypes(req, res) {
+  res.json(validTypes);
+}
+
+function handleGetPokemon(req, res) {
+  let response = POKEDEX.pokemon;
+
+  if(req.query.name) {
+    response = response.filter(pokemon => 
+      pokemon.name.toLowerCase().includes(req.query.name.toLowerCase())
+    );
+  }
+
+  if (req.query.type) {
+    response = response.filter(pokemon =>
+      pokemon.type.includes(req.query.type)
+    )
+  }
+  res.json(response)
+}
+
+app.get('/types', handleGetTypes);
+
+app.get('/pokemon', handleGetPokemon)
+
 
 app.listen(8000, () => {
-  console.log('app is listening on port 8000')
-})
+  console.log('app is listening on port 8000');
+});
  
 
 
