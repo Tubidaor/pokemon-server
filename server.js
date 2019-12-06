f68f649132cddba1e7c8129ed56bb2da7812c408
Juan Baltazar <juan.baltazar1@gmail.com>
2019-12-05 22:54:58 -0800

    ready for deployment

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
 require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
+const helmet = require('helmet');
+const cors = require('cors');
 const POKEDEX = require('./pokedex.json');
 
 const app = express();
-
-app.use(morgan('dev'));
-
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common'
+app.use(morgan(morganSetting));
+app.use(helmet());
+app.use(cors());
 app.use(function validateBearerToken(req, res, next) {
   const apiToken = process.env.API_TOKEN
   const authToken = req.get('Authorization')
-  console.log(authToken)
   
   if (!authToken || authToken.split(' ')[1] !== apiToken) {
     return res.status(401).json({ error: 'Unauthorized request' })
@@ -47,10 +49,8 @@ app.get('/types', handleGetTypes);
 
 app.get('/pokemon', handleGetPokemon)
 
+const PORT = process.env.PORT || 8000
+app.listen(PORT, () => { });
 
-app.listen(8000, () => {
-  console.log('app is listening on port 8000');
-});
- 
 
 
